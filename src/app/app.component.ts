abe0a4046e644b440ac1e7c5949ffd86cb457848
akihiko.kigure <akihiko.kigure@gmail.com>
2023-04-30 16:16:35 +0900

    app.component.ts stand alone 化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import 'zone.js/dist/zone.js';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'stand-alone-components-sample';
+  title = 'stand alone components sample';
 }
+
+bootstrapApplication(AppComponent).catch((error) => console.error(error));
