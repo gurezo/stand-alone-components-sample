d70ecfa906c40257640e2f0e191b6c522075dd9e
akihiko.kigure <akihiko.kigure@gmail.com>
2023-04-30 17:33:52 +0900

    list list-item 実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import 'zone.js/dist/zone.js';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+
+const componentList = [ListComponent, ListItemComponent] as const;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, componentList],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
