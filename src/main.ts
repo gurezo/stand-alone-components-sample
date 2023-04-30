3a5de90e368e5908a3c24f1137de52cef8070f71
akihiko.kigure <akihiko.kigure@gmail.com>
2023-04-30 16:16:54 +0900

    main.ts 暫定変更

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// import { AppComponent } from './app/app.component';
+
+import 'zone.js/dist/zone.js';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css'],
+})
+export class AppComponent {
+  title = 'stand alone components sample';
+}
+
+bootstrapApplication(AppComponent).catch((error) => console.error(error));
