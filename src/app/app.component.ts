955e074f2a3434e6502b96f2a05328fb4ee169aa
akihiko.kigure <akihiko.kigure@gmail.com>
2023-05-01 16:48:42 +0900

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import 'zone.js/dist/zone.js';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-// import { ListComponent } from './list/list.component';
-// import { ListItemComponent } from './list-item/list-item.component';
 
-// const componentList = [ListComponent, ListItemComponent] as const;
 import { ListModule } from './list/list.module';
 
 @Component({
