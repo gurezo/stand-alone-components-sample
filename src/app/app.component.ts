75b73c1675e56af2c02722550e5a0a5fbcbf57f3
akihiko.kigure <akihiko.kigure@gmail.com>
2023-05-05 15:33:25 +0900

    app 編集

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import 'zone.js/dist/zone.js';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ListComponent],
+  imports: [CommonModule, RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
