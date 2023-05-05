920b92fa8c4c428fa5241670415f4b858ad99449
akihiko.kigure <akihiko.kigure@gmail.com>
2023-05-05 15:26:29 +0900

    ルーティング実装

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
-}).catch((error) => console.error(error));
+bootstrapApplication(AppComponent, appConfig).catch((error) =>
+  console.error(error)
+);
