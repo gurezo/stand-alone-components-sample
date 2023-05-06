512d4c9e74762f65f07aaeacba37e0a532ccb839
akihiko.kigure <akihiko.kigure@gmail.com>
2023-05-06 17:22:33 +0900

    stand alone service unit test

--- src/app/list/list.service.spec.ts
+++ src/app/list/list.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 
 import { ListService } from './list.service';
+import { of } from 'rxjs';
 
-describe('ListService', () => {
+fdescribe('ListService', () => {
   let service: ListService;
+  let controller: HttpTestingController;
+  const apiResponseValue = [{ name: 'hoge' }, { name: 'fuga' }];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting(), ListService],
+    });
     service = TestBed.inject(ListService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get user info from backend API', async () => {
+    spyOn(service, 'get').and.callFake(() => of(apiResponseValue));
+    service.get().subscribe((_) => expect(_).toEqual(apiResponseValue));
   });
 });
