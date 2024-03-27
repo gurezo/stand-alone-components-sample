3b3d3c06009b0dc6a83a64703a9b2d745bada63e
akihiko.kigure <akihiko.kigure@gmail.com>
2024-03-27 21:41:42 +0900

    default route app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
+export default [
   {
     path: '',
-    loadChildren: () => import('./parent.routes').then((m) => m.parentRoutes),
+    loadChildren: () => import('./parent/parent.routes'),
   },
 ];
