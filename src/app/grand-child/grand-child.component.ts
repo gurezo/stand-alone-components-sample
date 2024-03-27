b701c0801c87fe3a9a15328d81b56a2a4a9cdd6e
akihiko.kigure <akihiko.kigure@gmail.com>
2024-03-27 21:40:28 +0900

    default component grand-child

--- src/app/grand-child/grand-child.component.ts
+++ src/app/grand-child/grand-child.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grand-child',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './grand-child.component.html',
-  styleUrls: ['./grand-child.component.css']
+  styleUrls: ['./grand-child.component.css'],
 })
-export class GrandChildComponent {
-
-}
+export default class GrandChildComponent {}
