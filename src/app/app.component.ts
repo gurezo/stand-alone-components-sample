57f8b463e975089a39e97f2fb45389a9b5650076
akihiko.kigure <akihiko.kigure@gmail.com>
2024-03-27 21:44:56 +0900

    remove zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import 'zone.js/dist/zone.js';
 
 import { ListComponent } from './list/list.component';
 import { CompositionNgifDirective } from './shared/directive/composition-ngif.directive';
